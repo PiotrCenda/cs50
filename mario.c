d3616680338c79b038278fd3b1860ec318d5b97f
PiotrCenda <PiotrCenda@users.noreply.github.com>
2020-03-29 18:37:30 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -4,29 +4,33 @@
 int main(void)
 {
     int wys = 0;
-    do{
+
+    do
+    {
         wys = get_int("Height: ");
-    }while(wys>8 || wys<1);
+    }
+    while(wys>8 || wys<1);
 
     int i, j;
 
-    for(i=0; i<wys; i++)
+    for(i = 0; i < wys; i++)
     {
-        for(j=0; j<wys+i+1; j++)
+        for(j = 0; j < wys + i + 1; j++)
         {
-            if((j>=wys-i-1 && j<=wys) || j>=wys)
+            if((j >= wys - i - 1 && j <= wys) || j >= wys)
             {
                 printf("#");
             }
-            else if(j!=wys-1)
+            else if(j != wys - 1)
             {
                 printf(" ");
             }
-            if(j==wys-1)
+            if(j == wys - 1)
             {
                 printf("  ");
             }
         }
+        
         printf("\n");
     }
 }
