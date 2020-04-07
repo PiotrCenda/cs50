262b297a61a706d93e3c4e19da5380c75866cd3f
PiotrCenda <PiotrCenda@users.noreply.github.com>
2020-04-07 20:55:45 +0000

    automated commit by check50 [check50=True]

--- readability.c
+++ readability.c
@@ -30,14 +30,14 @@ int main(void)
 
     if (index < 1)
     {
-        printf("Before Grade 1");
+        printf("Before Grade 1\n");
     }
     else if (index >= 16)
     {
-        printf("Grade 16+");
+        printf("Grade 16+\n");
     }
     else
     {
-        printf("Grade %i", (int) round(index));
+        printf("Grade %i\n", (int) round(index));
     }
 }
