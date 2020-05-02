3961d045bca72d20badc924a6516e14df2746f0c
PiotrCenda <PiotrCenda@users.noreply.github.com>
2020-05-02 16:13:07 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -6,4 +6,4 @@ while n < 1 or n > 8:
     n = get_int("Heigth: ")
 
 for i in range(1, n + 1):
-    print(" " * (n - i) + "#" * i + " " + "#" * i)
+    print(" " * (n - i) + "#" * i + "  " + "#" * i)
